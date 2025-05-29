4a35b04618c9d2d774e31f7db62e7494a328296b
Manav Bhagwani <manavbha@cisco.com>
2025-05-29 22:57:53 +0530

    webhook testing

--- src/routes/error.js
+++ src/routes/error.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.use((req, res) => {
-  res.status(404).send({ error: "not found" });
+  res.status(404).send({ error: "not found1" });
 });
 
 module.exports = router;
